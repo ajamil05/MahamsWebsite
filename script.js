399e69f0fbfccd091a41eec0fe430c2bfdd3d4af
Abbas Jamil <123810258+ajamil05@users.noreply.github.com>
2026-08-06 19:14:24 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+// Smooth scrolling
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+
+    link.addEventListener("click", function(e){
+
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute("href"))
+        .scrollIntoView({
+
+            behavior:"smooth"
+
+        });
+
+    });
+
+});
+
+
+console.log("Maham Khalid Portfolio Loaded");

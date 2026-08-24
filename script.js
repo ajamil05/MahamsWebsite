166e324a414d9cc4f988df10af41ca6d665058e2
Abbas Jamil <123810258+ajamil05@users.noreply.github.com>
2026-08-24 14:20:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-// Smooth scrolling
-function openPopup() {
-    document.getElementById("popup").style.display = "flex";
-}
-
-function closePopup() {
-    document.getElementById("popup").style.display = "none";
-}
-
-// Close when clicking outside the popup
-window.onclick = function(event) {
-    const popup = document.getElementById("popup");
-
-    if (event.target === popup) {
-        closePopup();
-    }
-}
 document.querySelectorAll('a[href^="#"]').forEach(link => {
 
     link.addEventListener("click", function(e){
